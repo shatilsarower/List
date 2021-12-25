ce08cb09376ff79edcdc2a4a0459f6f433b6b332
sms <shatild12@gmail.com>
2021-12-25 16:58:18 +0600

    The error1

--- lib/Classess/My_Use_Data.dart
+++ lib/Classess/My_Use_Data.dart
@@ -1,11 +1,1 @@
-class MyUserdata {
-  String? username;
-  String? userPhone;
-  String? userEmail;
 
-  MyUserdata({
-    required this.username,
-    required this.userPhone,
-    required this.userEmail,
-  });
-}
